a9201c01a4657c831c51575a3d59c552496d270c
tjellicoe <thomas.jellicoe@telespazio.com>
2024-11-12 15:29:51 +0000

    Correct key name

--- {{cookiecutter.service_name}}/service.py
+++ {{cookiecutter.service_name}}/service.py
@@ -487,9 +487,8 @@ def {{cookiecutter.workflow_id |replace("-", "_")  }}(conf, inputs, outputs): # 
         calling_service_account = calling_workspace.get("spec", {}).get("serviceAccount", {}).get("name", "default")
 
         # Set calling namespace to allow PVC to be mounted
-        inputs.update({"update_namespace": {"value": calling_workspace["spec"]["namespace"]}})
+        inputs.update({"calling_namespace": {"value": calling_workspace["spec"]["namespace"]}})
 
-        
         if calling_workspace_name == executing_workspace_name:
             executing_service_account = calling_service_account
             executing_namespace = calling_workspace["spec"]["namespace"]
