966d4777305cf0c9db30bcc3d4c9454958e8c2cb
tjellicoe <thomas.jellicoe@telespazio.com>
2024-12-16 13:43:07 +0000

    Update keycloak client env var name

--- {{cookiecutter.service_name}}/service.py
+++ {{cookiecutter.service_name}}/service.py
@@ -490,8 +490,8 @@ def {{cookiecutter.workflow_id |replace("-", "_")  }}(conf, inputs, outputs): # 
         conf.setdefault("eodhp", {})
         conf["eodhp"]["serviceAccountName"] = "default"
 
-        inputs["ADES_KEYCLOAK_CLIENT_SECRET"] = {}
-        inputs["ADES_KEYCLOAK_CLIENT_SECRET"]["value"] = os.getenv("ADES_KEYCLOAK_CLIENT_SECRET")
+        inputs["KEYCLOAK_CLIENT_SECRET"] = {}
+        inputs["KEYCLOAK_CLIENT_SECRET"]["value"] = os.getenv("KEYCLOAK_CLIENT_SECRET")
 
         execution_handler = EoepcaCalrissianRunnerExecutionHandler(conf=conf, inputs=inputs)
 
